52c499a9eb40c0e5b7ec3bfa98439e0b368eddda
bovineblitzkrieg <116898818+bovineblitzkrieg@users.noreply.github.com>
2023-05-23 19:47:30 -0400

    Add files via upload

---
+++ lightningcracker.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         LightningCracker
+// @namespace    bov
+// @version      1.0
+// @description  Repeatedly adds lightning deal items to cart. Toggle on and off via hotkey (line 15).
+// @author       bovine - original console script by Keretus
+// @match        https://www.amazon.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var hotkey = '0'; // Hotkey value, e.g., '0'
+    var running = false; // Flag to track if the script is running
+
+    // Function to add items to cart
+    function addToCart() {
+        if (document.querySelectorAll("[id^='dealsx_incart']")[0].classList.contains('aok-hidden')) {
+            document.querySelectorAll("[id^='dealsx_atc_btn']")[0].click();
+            setTimeout(addToCart, 1500); // delay between attempts in ms (1000 = 1 second)
+        }
+    }
+
+    // Hotkey toggle
+    function handleHotkey(event) {
+        if (event.key === hotkey) {
+            if (running) {
+                // Stop the script if it is already running
+                running = false;
+                console.log('Script stopped');
+            } else {
+                // Start the script if it is not running
+                running = true;
+                console.log('Script started');
+                addToCart();
+            }
+        }
+    }
+
+    // Hotkey listener
+    document.addEventListener('keydown', handleHotkey);
+})();
